790b1f002c7ad2f5831d70068add7e96a697c19e
TashZaman <58598307+TashZaman@users.noreply.github.com>
2019-12-06 10:12:25 -0400

    Update and rename README.md to README.java

---
+++ README.java
@@ -0,0 +1,13 @@
+public class helloWorld {
+
+    public static void main(String[] args)  {
+    
+        System.out.println("Hello World");
+        
+    }
+    
+}
+
+My first repository.
+
+I don't know if I'm "gitting" it lol
--- README.md
+++
@@ -1,4 +0,0 @@
-# hello-world
-My first repository.
-
-I don't know if I'm "gitting" it lol
